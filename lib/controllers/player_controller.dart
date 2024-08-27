c9271f01c6f64d5ad92ad00e8a2e61d82772c4ae
logossoudarryl20 <logossoudarryl20@gmail.com>
2024-08-27 11:17:50 +0000

    controller

---
+++ lib/controllers/player_controller.dart
@@ -0,0 +1,28 @@
+import 'package:get/get.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class PlayerController extends GetxController {
+  final audioQuery = new OnAudioQuery();
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    checkPermission();
+  }
+
+  checkPermission() async {
+    var perm = await Permission.storage.request();
+    if (perm.isGranted) {
+      // return audioQuery.querySongs(
+      //   ignoreCase: true,
+      //   orderType: OrderType.ASC_OR_SMALLER,
+      //   sortType: null,
+      //   uriType: UriType.EXTERNAL,
+      // );
+    } else {
+      checkPermission();
+    }
+  }
+}
