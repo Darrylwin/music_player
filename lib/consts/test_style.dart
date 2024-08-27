96674ec77a395da4832f6961406f72c135ef0f51
logossoudarryl20 <logossoudarryl20@gmail.com>
2024-08-27 08:32:06 +0000

    textstyle(general)

---
+++ lib/consts/test_style.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/consts/colors.dart';
+
+const bold = "bold";
+const regular = "regular";
+ourStyle({
+  family = regular,
+  double? size = 14,
+  color = whiteColor,
+}) {
+  return TextStyle(
+    fontSize: size,
+    color: color,
+    fontFamily: family,
+  );
+}
