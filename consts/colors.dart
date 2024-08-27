137a2a5e299e1a276693e6de19a293d3f56ce196
logossoudarryl20 <logossoudarryl20@gmail.com>
2024-08-27 07:51:54 +0000

    colors setted

---
+++ consts/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const bgColor = Color(0xFF373740);
+const whiteColor = Color(0xFFF2F2F2);
+const sliderColor = Color(0xFF07D950);
+const buttonColor = Color(0xFF07D962);
+const bgDarkColor = Color(0xFF0B0B0D);
