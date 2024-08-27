5ced4e077796a289e02b67fa3a03724b12e7debe
logossoudarryl20 <logossoudarryl20@gmail.com>
2024-08-27 11:16:59 +0000

    cbackground color modified

--- lib/consts/colors.dart
+++ lib/consts/colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-const bgColor = Color(0xFF373740);
+// const bgColor = Color.fromARGB(255, 15, 15, 23);
+const bgColor = Color.fromARGB(255, 30, 30, 35);
 const whiteColor = Color(0xFFF2F2F2);
 const sliderColor = Color(0xFF07D950);
 const buttonColor = Color(0xFF07D962);
